09ae3626b428395a43c54375646505584e67c3aa
Matt Weagle <mweagle@gmail.com>
2016-06-08 22:54:38 -0700

    Update paths

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ import (
 	spartaCF "github.com/mweagle/Sparta/aws/cloudformation"
 	spartaIAM "github.com/mweagle/Sparta/aws/iam"
 
-	"github.com/mweagle/SpartaCICD/concourse"
-	"github.com/mweagle/SpartaCICD/resources"
+	"./concourse"
+	"./resources"
 	"github.com/spf13/cobra"
 	"net/http"
 	"os"
